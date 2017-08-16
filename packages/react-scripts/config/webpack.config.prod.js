f4b569ebee49b80674f6b210348ff86f1795ba53
huangyan <huangyan@xsky.com>
2017-08-16 12:21:17 +0800

    Add po-loader

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -250,6 +250,14 @@ module.exports = {
             ),
             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
+          // "po" loader convert po file to json, used by i18n module.
+          {
+            test: /\.po$/,
+            use: [
+              require.resolve('json-loader'),
+              require.resolve('po-loader'),
+            ],
+          },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader don't uses a "test" so it will catch all modules
