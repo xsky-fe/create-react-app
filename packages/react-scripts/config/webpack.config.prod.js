a096e67b99c36c7d8fa062fedf9a115a14be8123
jianxin <jianxin@xsky.com>
2019-07-08 17:50:24 +0800

    feat: Copy chrome installer to package.

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -508,7 +508,11 @@ module.exports = {
       {
         from: path.join(paths.appAsset, 'javascripts/browser-ua.js'),
         to: path.join(paths.appBuild, 'static/js/browser-ua.js'),
-      }
+      },
+      {
+        from: path.join(paths.appAsset, 'browser'),
+        to: path.join(paths.appBuild, 'static/browser'),
+      },
     ]),
     // Generates an `index.html` file with the <script> injected.
     new HtmlWebpackPlugin({
